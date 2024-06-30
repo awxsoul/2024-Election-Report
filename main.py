d7e8bc105e42bf1165ad360f3b6bd9852011eb32
Gopi Krishna <awxsoul8@gmail.com>
2024-06-30 12:39:46 +0530

    Access the website
    Add script to access and establish the connection with the Election Commission website

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # The libraries used here are requests, beautifulsoup4 and pandas
+import requests
+from bs4 import BeautifulSoup
+
+url='https://results.eci.gov.in'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify()) #to visually understand html content better
