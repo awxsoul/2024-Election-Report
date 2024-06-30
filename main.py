0c200d72f94c249b8724e7323565fba64555810e
Gopi Krishna <awxsoul8@gmail.com>
2024-06-30 18:16:02 +0530

    Navigate to all end pages
    Selenium is used to navigate to all the end leaf pages of the Parliamentary Constituencies Page.

--- main.py
+++ main.py
@@ -1,9 +1,61 @@
 # The libraries used here are requests, beautifulsoup4 and pandas
+import time
 import requests
 from bs4 import BeautifulSoup
 
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
+#main base page
 url='https://results.eci.gov.in'
 response = requests.get(url)
-
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.prettify()) #to visually understand html content better
+
+#soup of Parliamentary Constituencies Page
+pc_url=soup.find_all('a')[1].get('href') #the second link is to Parliamentary Constituencies
+pc_response = requests.get(pc_url)
+pc_soup = BeautifulSoup(pc_response.text, 'html.parser')
+
+#use of selenium driver to navigate and find info
+driver = webdriver.Chrome('./chromedriver.exe')
+
+#get all the state lists from dropdown menu
+pc_links=[]
+for state in (pc_soup.find_all('option')):
+    pc_links.append([state.text,])
+pc_links.pop(0)
+
+#to navigate driver to state pages
+for state_index in range(0,len(pc_links)):
+    driver.get(pc_url)
+    select= Select(driver.find_element('name','state')) #locate the dropdown menu
+    select.select_by_visible_text(pc_links[state_index][0]) #selecting options
+    pc_links[state_index].append(driver.current_url)#get url after navigating to the page
+
+    #PARSE for Constituency
+    #soup of state pages
+    st_url=driver.current_url
+    st_response = requests.get(st_url)
+    st_soup = BeautifulSoup(st_response.text, 'html.parser')
+
+    #get all the constituency lists from dropdown menu
+    pc_links[state_index].append([])
+    for constituency in (st_soup.find_all('option')):
+         c_name=' '.join(map(str,constituency.text.split()))
+         print(c_name)
+         pc_links[state_index][2].append(c_name)
+    pc_links[state_index][2].pop(0)
+    print(pc_links[state_index][2])
+    
+    #to navigate driver to constituency pages
+    for constituency_index in range(0,len(pc_links[state_index][2])):
+        driver.get(st_url)
+        select= Select(driver.find_element('name','state')) #locate the dropdown menu
+        select.select_by_visible_text(pc_links[state_index][2][constituency_index]) #selecting options
+
+        print(pc_links[state_index][0],pc_links[state_index][2][constituency_index],"fine")
+   
+
+print(pc_links)
+
+driver.close()
